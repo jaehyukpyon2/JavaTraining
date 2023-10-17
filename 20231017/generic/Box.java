db2913585cfb876b54f6ffd8c2442c05031ad5c7
jaehyukpyon <ipadforjay@gmail.com>
2023-10-17 16:43:37 +0900

    generic and thread

---
+++ 20231017/generic/Box.java
@@ -0,0 +1,14 @@
+package _20231017.generic;
+
+public class Box<T> {
+
+    private T t;
+
+    public T get() {
+        return t;
+    }
+
+    public void set(T t) {
+        this.t = t;
+    }
+}
